e8dd957ef0547a2828344a7e4cf88abd458ee7d8
Souleymane BA <ba.souleymane313@gmail.com>
2020-04-30 23:08:45 +0200

    Implemented some tests for lexer

---
+++ test/LexerTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by souls on 30/04/2020.
+//
+
+#include "LexerTest.h"
---
+++ test/LexerTest.h
@@ -0,0 +1,16 @@
+//
+// Created by souls on 30/04/2020.
+//
+
+#ifndef BARZANE_LEXERTEST_H
+#define BARZANE_LEXERTEST_H
+
+
+
+class LexerTest {
+
+};
+
+
+
+#endif //BARZANE_LEXERTEST_H
