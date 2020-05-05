271492a8344dc87e9e89d53b80bd5272b5786363
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-05 11:16:35 +0200

    Make prototype methods

---
+++ src/Parser.cpp
@@ -0,0 +1,30 @@
+//
+// Created by souls on 04/05/2020.
+//
+
+#include "../include/Parser.h"
+
+Parser::Parser(Lexer &lexer) {
+
+}
+
+Parser::~Parser() {
+
+}
+
+AST *Parser::getTree() const {
+    return nullptr;
+}
+
+void Parser::display() {
+
+}
+
+void Parser::displayErrors() {
+
+}
+
+bool Parser::hasErrors() {
+    return false;
+}
+
