78bcf9ad4a49ebde750ce8dba9c191963680b885
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-11 17:45:37 +0200

    Make pointer to indexer

--- test/src/NumberTest.cpp
+++ test/src/NumberTest.cpp
@@ -6,64 +6,70 @@
 
 void NumberTest::getNumber_When_IndexerIsEmpty() {
     vector<Symbol*> empty = {};
-    Indexer<Symbol> indexer = Indexer<Symbol>(empty);
-    Number number = Number(indexer);
-    number.construct();
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(empty);
+    Number* number = new Number(indexer);
+    number->construct();
 
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is undefined", number.getType() == NumberType::UNDEFINED);
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is undefined", number->getType() == NumberType::UNDEFINED);
+    
+    delete number;
 }
 
 void NumberTest::getNumber_When_CurrentIsNotCatchable() {
     string x = "10x";
     Symbol* id = new Symbol(x, 1, 1);
     vector<Symbol*> empty = {id};
-    Indexer<Symbol> indexer = Indexer<Symbol>(empty);
-    Number number = Number(indexer);
-    number.construct();
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(empty);
+    Number* number = new Number(indexer);
+    number->construct();
 
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is undefined", number.getType() == NumberType::UNDEFINED);
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is undefined", number->getType() == NumberType::UNDEFINED);
+    
+    delete number;
 }
 
 void NumberTest::getNumber_When_CurrentIsDigit() {
     string integer = "1000";
     Symbol* value = new Symbol(integer, 1, 1);
     vector<Symbol*> buf = {value};
-    Indexer<Symbol> indexer = Indexer<Symbol>(buf);
-    Number number = Number(indexer);
-    number.construct();
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(buf);
+    Number* number = new Number(indexer);
+    number->construct();
 
-    expect("The value should be 1000", *number.getInteger() == 1000);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is INTEGER", number.getType() == NumberType::INTEGER);
+    expect("The value should be 1000", *number->getInteger() == 1000);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is INTEGER", number->getType() == NumberType::INTEGER);
+    
+    delete number;
 }
 
 void NumberTest::getNumber_When_CurrentIsReal() {
     string real = "1000.12";
     Symbol* value = new Symbol(real, 1, 1);
     vector<Symbol*> buf = {value};
-    Indexer<Symbol> indexer = Indexer<Symbol>(buf);
-    Number number = Number(indexer);
-    number.construct();
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(buf);
+    Number* number = new Number(indexer);
+    number->construct();
 
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("The value should be 1000.12", *number.getReal() == 1000.12f);
-    expect("Type number is REAL", number.getType() == NumberType::REAL);
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("The value should be 1000.12", *number->getReal() == 1000.12f);
+    expect("Type number is REAL", number->getType() == NumberType::REAL);
 }
 
 void NumberTest::getNumber_When_NotConstruct() {
     string value = "152";
     Symbol *any = new Symbol(value, 1, 15);
     vector<Symbol*> symbols = {any};
-    Indexer<Symbol> indexer = Indexer<Symbol>(symbols);
-    Number number = Number(indexer);
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(symbols);
+    Number* number = new Number(indexer);
 
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is undefined", number.getType() == NumberType::UNDEFINED);
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is undefined", number->getType() == NumberType::UNDEFINED);
 }
 
 void NumberTest::getNumber_When_BadReal() {
@@ -72,31 +78,31 @@ void NumberTest::getNumber_When_BadReal() {
     Symbol* leftSymbol = new Symbol(left, 1, 15);
     Symbol* rightSymbol = new Symbol(right, 1568, 157);
     vector<Symbol*> symbols = {leftSymbol, rightSymbol};
-    Indexer<Symbol> indexer = Indexer<Symbol>(symbols);
-    Number number = Number(indexer);
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(symbols);
+    Number* number = new Number(indexer);
 
-    number.construct();
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is undefined", number.getType() == NumberType::UNDEFINED);
+    number->construct();
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is undefined", number->getType() == NumberType::UNDEFINED);
 
-    number.construct();
-    expect("Pointer of integer should be 0", number.getInteger() == 0);
-    expect("Pointer of real should be 0", number.getReal() == 0);
-    expect("Type number is undefined", number.getType() == NumberType::UNDEFINED);
+    number->construct();
+    expect("Pointer of integer should be 0", number->getInteger() == 0);
+    expect("Pointer of real should be 0", number->getReal() == 0);
+    expect("Type number is undefined", number->getType() == NumberType::UNDEFINED);
 }
 
 void NumberTest::positionChanged_WhenConstruct() {
     string value = "\"152\"";
     Symbol *any = new Symbol(value, 1, 15);
     vector<Symbol*> symbols = {any};
-    Indexer<Symbol> indexer = Indexer<Symbol>(symbols);
-    Number number = Number(indexer);
-    unsigned int previous = indexer.position();
+    Indexer<Symbol>* indexer = new Indexer<Symbol>(symbols);
+    Number* number = new Number(indexer);
+    unsigned int previous = indexer->position();
 
-    number.construct();
+    number->construct();
 
-    unsigned int current = indexer.position();
+    unsigned int current = indexer->position();
 
     expect("Current index should be equal the previous added 1", previous == current - 1);
 }
