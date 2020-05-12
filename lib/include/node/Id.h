3b0b06d0ef47756217db078d8b28bc1290f4f409
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-12 20:39:37 +0200

    Document Id and Ids nodes

--- lib/include/node/Id.h
+++ lib/include/node/Id.h
@@ -8,19 +8,50 @@
 
 #include "../Node.h"
 
+
 /**
- * id: [a-zA-Z_][a-zA-Z0-9_]*
- */
+* A node id class is used to represent a login that may a name
+* of variable or function of the Barzane abstract syntax grammar.
+* Its definition looks like:
+*      id: [a-zA-Z_][a-zA-Z0-9_]*
+*/
 class Id: public Node {
 
     public:
+        /**
+         * Constructor
+         *
+         * @param tokens: indexer of a set of tokens.
+         */
         Id(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         virtual ~Id();
 
+        /**
+         * This method recover a login from the current token in
+         * checking first this one respect Barzane grammar id. If this
+         * latter is not matched, it raises an error. In the end, it points
+         * the indexer on the next token.
+         */
         void construct();
 
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node id.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get the recovered login
+         *
+         * @return a string.
+         */
         string *getName() const;
 
     private:
--- lib/include/node/Ids.h
+++ lib/include/node/Ids.h
@@ -10,16 +10,56 @@
 #include "Id.h"
 #include <string>
 
+/**
+* A node ids class is used to represent a set of node id
+* of the Barzane abstract syntax grammar. Its definition looks like:
+*      ids: {id} | {id} "," {ids}
+*/
 class Ids: public Node {
 
     public:
+        /**
+         * Constructor
+         *
+         * @param tokens: indexer of a set of tokens.
+         */
         Ids(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         virtual ~Ids();
 
+        /**
+         * This methods builds a set of node id in checking
+         * first if the current token is an id. If it is not the
+         * case, it raises an error. it gets recursively all node id while
+         * the next token after each id is a coma. In the end, it points
+         * on the next token regarding the indexer.
+         */
         void construct();
+
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node ids.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get the recovered id.
+         *
+         * @return a node id.
+         */
         Id *getId() const;
+
+        /**
+         * Get the recovered ids.
+         *
+         * @return a node ids.
+         */
         Ids *getIds() const;
 
     private:
