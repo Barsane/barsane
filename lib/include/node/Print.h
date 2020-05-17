55ad8b2347189436274d1c0644500e071b4660f2
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-17 17:05:48 +0200

    Documents print

--- lib/include/node/Print.h
+++ lib/include/node/Print.h
@@ -8,16 +8,50 @@
 #include "../Node.h"
 #include "Expression.h"
 
+/**
+* A node print class is used to print an expression
+* of the Barzane abstract syntax grammar.
+* Its definition looks like:
+*       print: "print" {expression} ";"
+*/
 class Print: public Node {
 
     public:
+        /**
+         * Construct
+         *
+         * @param tokens: a indexer of a set of tokens.
+         */
         Print(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         virtual ~Print();
 
+        /**
+         * It checks if the current token is 'print' keyword
+         * then it recovers the next token if it is an expression.
+         * The fact these conditions are not met, raises an error.
+         * In the end, a semicolon must be matched otherwise an error
+         * kind of expect ';' at the end is throw.
+         */
         void construct();
 
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node print.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get expression.
+         *
+         * @return a node expression.
+         */
         Expression *getExpression() const;
 
     private:
