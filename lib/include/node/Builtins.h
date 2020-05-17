363719a2950d53680d93e90359cf6141fd153920
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-17 17:45:25 +0200

    Implement node builtins doc

--- lib/include/node/Builtins.h
+++ lib/include/node/Builtins.h
@@ -8,15 +8,48 @@
 #include "../Node.h"
 #include "Print.h"
 
+/**
+* A node builtins class is used to represent a set of builtins functions
+* of the Barzane abstract syntax grammar.
+* Its definition looks like:
+*     builtins: {print}
+*/
 class Builtins: public Node {
 
     public:
+        /**
+         * Constructor
+         *
+         * @param tokens: a indexer of a set of tokens.
+         */
         Builtins(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         ~Builtins();
 
+        /**
+         * It checks if the current token is one of the keywords
+         * regarding the builtins functions defined by the grammar
+         * Barzane. If it is met, it gets the corresponding node.
+         */
         void construct();
+
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node builtins.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get print.
+         *
+         * @return a node print.
+         */
         Print *getPrint() const;
 
     private:
