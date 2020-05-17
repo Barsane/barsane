db5360db67d0146d66cf51e672d8a9aefdade180
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-17 15:37:02 +0200

    Wrote doc for nodes affect and affects

--- lib/include/node/Affect.h
+++ lib/include/node/Affect.h
@@ -10,16 +10,55 @@
 #include "Id.h"
 #include "Expression.h"
 
+/**
+* A node affect class is used to represent a assignment value
+* to a variable of the Barzane abstract syntax grammar.
+* Its definition looks like:
+*      affect: {id} "=" {expression} ";"
+*/
 class Affect: public Node {
     public:
+       /**
+         * Construct
+         *
+         * @param tokens: a indexer of a set of tokens.
+         */
         Affect(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor.
+         */
         virtual ~Affect();
 
+        /**
+         * It walks the indexer to extracting the nodes id and expression
+         * that defines a node. It might either node that be not set if
+         * the grammar is not caught. In this case, this method raises
+         * an error. In the end, the current index point at the beginning
+         * of the next instruction meaning after the next semicolon token.
+         */
         void construct();
+
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node affect.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get id.
+         * @return a node id.
+         */
         Id* getId() const;
 
+        /**
+         * Get expression.
+         *
+         * @return a node expression.
+         */
         Expression *getExpression() const;
 
     private:
--- lib/include/node/Affects.h
+++ lib/include/node/Affects.h
@@ -8,16 +8,56 @@
 #include "../Node.h"
 #include "Affect.h"
 
+/**
+* A node affects class is used to represent a set of node affect
+* of the Barzane abstract syntax grammar. Its definition looks like:
+*      affects: {affect} | {affect} {affects}
+*/
 class Affects: public Node {
 
     public:
+        /**
+         * Construct
+         *
+         * @param tokens: a indexer of a set of tokens.
+         */
         Affects(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         ~Affects();
 
+       /**
+         * This methods builds a set of node affect in checking
+         * first if the current and next tokens are respectively an id and equal.
+         * If it is not the case, it raises an error. Then, it gets recursively all
+         * node affect while catching a affect grammar.
+         * In the end, it points on the next token regarding the indexer.
+         */
         void construct();
+
+       /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node affects.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get affect.
+         *
+         * @return a node affect.
+         */
         Affect *getAffect() const;
+
+        /**
+         * Get affects.
+         *
+         * @return a node affects.
+         */
         Affects *getAffects() const;
 
     private:
