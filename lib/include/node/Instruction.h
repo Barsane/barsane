489b374ff2253e3c26a1f8e0c69ff0907e9a9c9f
Souleymane BA <ba.souleymane313@gmail.com>
2020-05-18 19:31:10 +0200

    Document node instruction

--- lib/include/node/Instruction.h
+++ lib/include/node/Instruction.h
@@ -10,22 +10,66 @@
 #include "Affects.h"
 #include "Builtins.h"
 
+/**
+* A node instruction class is used to define an instruction that can
+ * be a declaration, an assignment or a call to a builtin function
+* of the Barzane abstract syntax grammar. Its definition looks like:
+*      instruction: {declarations} | {affects} | {builtins}
+*/
 class Instruction: public Node {
     public:
+        /**
+         * Construct
+         *
+         * @param tokens: an indexer of a set of tokens.
+         */
         Instruction(Indexer<Symbol>* tokens);
+
+        /**
+         * Destructor
+         */
         virtual ~Instruction();
 
+        /**
+         * It checks if the current token is a declaration or
+         * an assignment or a call builtin function. It if it is
+         * the case, it constructs the corresponding node. Otherwise
+         * it raises an error.
+         */
         void construct();
 
+
+        /**
+         * A json representative object.
+         *
+         * @param indentSize: it is by default 1 and defines the total space
+         * that represents an indentation for each json node
+         * @return a string that represents the object node instruction.
+         */
         const string json(unsigned int indentSize = 1) const;
 
+        /**
+         * Get declarations.
+         *
+         * @return a node declarations.
+         */
         Declarations *getDeclarations() const;
 
+        /**
+         * Get affects.
+         *
+         * @return a node affects.
+         */
         Affects *getAffects() const;
 
+        /**
+         * Get builtins.
+         *
+         * @return a node builtins.
+         */
         Builtins *getBuiltins() const;
 
-private:
+    private:
         Declarations* declarations;
         Affects* affects;
         Builtins* builtins;
